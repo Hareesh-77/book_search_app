43c64f3f352485e5fe923b2c42ac029b547ec630
Hareesh-77 <hareeshkondrupati@gmail.com>
2026-01-08 01:18:07 +0530

    Added available books

--- app.py
+++ app.py
@@ -10,30 +10,50 @@ st.write("Search books by **Book Name** or **Receiver Name**")
 try:
     df = pd.read_excel("books.xlsx")
 
+    available_books_df = df[df["RECIPIENT"].isna() | (df["RECIPIENT"].str.strip() == "")]
+
+    # Get unique available book names
+    available_books = sorted(available_books_df["BOOK NAME"].dropna().unique())
+
     col1, col2 = st.columns(2)
 
     with col1:
         search_type = st.selectbox(
             "Search Type",
-            ["BOOK NAME", "RECIPIENT"]
+            ["AVAILABLE BOOKS","BOOK NAME", "RECIPIENT"]
         )
 
     with col2:
-        query = st.text_input("Enter value")
+        if search_type == "AVAILABLE BOOKS":
+            selected_book = st.selectbox(
+                "Select Available Book",
+                available_books
+            )
+        else:
+            query = st.text_input("Enter value")
 
     st.divider()
 
-    if query:
-        if search_type == "BOOK NAME":
-            result = df[df["BOOK NAME"].str.contains(query, case=False, na=False)]
-        else:
-            result = df[df["RECIPIENT"].str.contains(query, case=False, na=False)]
+    # Logic
+    if search_type == "AVAILABLE BOOKS":
+        result = available_books_df[
+            available_books_df["BOOK NAME"] == selected_book
+        ]
 
-        if not result.empty:
-            st.success(f"{len(result)} record(s) found")
-            st.dataframe(result, use_container_width=True)
-        else:
-            st.warning("No records found")
+    elif search_type == "BOOK NAME" and query:
+        result = df[df["BOOK NAME"].str.contains(query, case=False, na=False)]
+
+    elif search_type == "RECIPIENT" and query:
+        result = df[df["RECIPIENT"].str.contains(query, case=False, na=False)]
+
+    else:
+        result = pd.DataFrame()
+        
+    if not result.empty:
+        st.success(f"{len(result)} record(s) found")
+        st.dataframe(result, use_container_width=True)
+    else:
+        st.warning("No records found")
 
 except Exception as e:
     st.error(f"Error occurred: {e}")
